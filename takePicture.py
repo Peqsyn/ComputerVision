419345285f7e2d546862101fa7361e4c51ac280b
Eric <ericlyly@hotmail.com>
2018-06-01 17:52:19 -0700

    cv2 image processing tools update

--- takePicture.py
+++ takePicture.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1) # video capture source camera (Here webcam of laptop) 
+cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
 
 while(1):
     ret,frame = cap.read() # return a single frame in variable `frame`
